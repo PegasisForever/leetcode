494bb31f9de0c0064095eec18b5bb25ddfa93a01
pegasis <admin@pegasis.site>
2020-09-14 02:53:45 -0400

    weekly contest 203

--- src/Weekly_Contest_203/c/Solution.kt
+++ src/Weekly_Contest_203/c/Solution.kt
@@ -2,33 +2,23 @@ package Weekly_Contest_203.c
 
 // timeout
 class Solution {
-    fun IntRange.length() = last - first + 1
-
     fun findLatestStep(arr: IntArray, m: Int): Int {
-        if (m == arr.size) return m
-        val totalRange = arr.indices
-        val rangeArr = Array<IntRange?>(arr.size) { totalRange }
-
-        for (arrI in arr.lastIndex downTo 1) {
-            val i = arr[arrI] - 1
-            val currRange = rangeArr[i]!!
-            rangeArr[i] = null
-
-            val pre = currRange.first until i
-            val post = (i + 1)..currRange.last
-
-            if (pre.length() == m || post.length() == m) {
-                return arrI
-            }
-
-            for (j in pre) {
-                rangeArr[j] = pre
-            }
-            for (j in post) {
-                rangeArr[j] = post
-            }
+        var res = -1
+        val n: Int = arr.size
+        val length = IntArray(n + 2)
+        val count = IntArray(n + 1)
+        for (i in 0 until n) {
+            val a: Int = arr[i]
+            val left = length[a - 1]
+            val right = length[a + 1]
+            length[a + right] = left + right + 1
+            length[a - left] = length[a + right]
+            length[a] = length[a - left]
+            count[left]--
+            count[right]--
+            count[length[a]]++
+            if (count[m] > 0) res = i + 1
         }
-
-        return -1
+        return res
     }
 }
